9941b90626b23081e1e947fe61d4230502b8032c
qiangyujun <qiangyujun@jd.com>
2021-11-15 14:17:45 +0800

    提交一次

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// 引入 css 文件
+import './css/index.css'
+// 引入字体样式
+import './icon/iconfont.css'
+
+const element = document.createElement('div')
+element.className = "iconfont yujun-ashbin"
+document.body.appendChild(element)
---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const { resolve } = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { DefinePlugin } = require('webpack')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        filename: 'build.js',
+        path: resolve(__dirname, 'build'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|jp?g|gif)$/i,
+                type: "asset/resource",
+                generator: {
+                    filename: "img/[name].[hash:4][ext]"
+                }
+            },
+            {
+                test: /\.(ttf|woff|woff2)$/i,
+                generator: {
+                    filename: "icon/[name].[hash:4][ext]"
+                }
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: "线上环境",
+            template: './src/public/index.html'
+        }),
+        new DefinePlugin({
+            BASE_URL: '"./"'
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: __dirname + '/src/public',
+                    globOptions: {
+                        ignore: [ // 配置不用copy的文件
+                            '**/index.html',
+                        ]
+                    },
+                    to:'./'
+                }
+            ]
+        })
+    ],
+    mode: 'development'
+}
